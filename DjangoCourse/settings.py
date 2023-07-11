b77675a9c9555fe4186f990edd3f61ee5ca67d74
Abdusharif0095 <abdurafiki.abdusharif@gmail.com>
2023-07-11 10:54:20 +0300

    ADD DB AND REQUIREMENTS

--- DjangoCourse/settings.py
+++ DjangoCourse/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5ml-gqxr2ni1+w==z4vtoy8ffvl9it#c&(=^@p38-!m19@ow^!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
